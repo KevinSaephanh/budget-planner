6b5c76b7c66174924c9ed13d7595e0917d0dc152
Kevin Saephanh <Kevin_Saephanh@yahoo.com>
2019-07-11 22:13:30 -0700

    Updates

---
+++ precache-manifest.a5f9a8118c7ee766f32a50c6c7e678c3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5994c35ffc9feb5c55a5704804485b97",
+    "url": "/Budget-Planner/index.html"
+  },
+  {
+    "revision": "b5dd474537c04f5c2c37",
+    "url": "/Budget-Planner/static/css/2.505e3219.chunk.css"
+  },
+  {
+    "revision": "dd7a9e10ef4025f5fb25",
+    "url": "/Budget-Planner/static/css/main.83ca1387.chunk.css"
+  },
+  {
+    "revision": "b5dd474537c04f5c2c37",
+    "url": "/Budget-Planner/static/js/2.1769b8ef.chunk.js"
+  },
+  {
+    "revision": "dd7a9e10ef4025f5fb25",
+    "url": "/Budget-Planner/static/js/main.eee17f62.chunk.js"
+  },
+  {
+    "revision": "a8518dd96ef8b53517f6",
+    "url": "/Budget-Planner/static/js/runtime~main.15c6a78a.js"
+  },
+  {
+    "revision": "75dee8301a962c1de0e681d68aa12842",
+    "url": "/Budget-Planner/static/media/logo.75dee830.png"
+  }
+]);
